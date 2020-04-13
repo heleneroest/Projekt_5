647def839607af224311ba79b714a87b472569c7
LauraProbst <lauraskprobst@gmail.com>
2020-04-13 17:12:47 +0200

    .

---
+++ javascript/striber.js
@@ -0,0 +1,8 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+
+const carouselImage = document.querySelectorAll('.carousel-slide img');
+
+//button//
+
+const fremBtn = document.querySelector('#fremBtn');
+const tilbageBtn = document.querySelector('#tilbageBtn');
