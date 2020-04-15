2ae327c0b9a0ccb0c70e3dfbadedb822c814c2e3
Icehansen <60363964+Icehansen@users.noreply.github.com>
2020-04-15 16:59:06 +0200

    Update vidstedu.js

--- javascript/vidstedu.js
+++ javascript/vidstedu.js
@@ -1,12 +1,14 @@
 console.log(document.body);
 
-var myImage = document.getElementById('mainImage');
-var imageArray = ["../billeder/gettyimages-176165730.jpg", "../billeder/scan_003.jpg", "../billeder/scan_007.jpg"]; //image container//
-var imageIndex = 1;
+// Var som betyder variabler er dataværdier)
+var myImage = document.getElementById('mainImage'); //mainImage for at koble sammen med HTML billede
+var imageArray = ["../billeder/gettyimages-176165730.jpg", "../billeder/scan_003.jpg", "../billeder/scan_007.jpg"]; //Imagearray image container-> det er det de billeder vi bruger i slideshow)
+var imageIndex = 1;   // tæller tallet placeringen af ​​billederne
 
+//Function får billeder til at skifte
 function changeImage(){
   myImage.setAttribute("src", imageArray[imageIndex]);
-  imageIndex++;
-  if (imageIndex > 2) {imageIndex = 0;}
+  imageIndex++; //++ som er operatorer betyder increment, betyder at jeg har tilføjet billeder)
+  if (imageIndex > 2) {imageIndex = 0;} // Det er kontrolstruktur -->
 
 }
