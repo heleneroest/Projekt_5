07bfc7012e51675131bb607afdbdacfd57b9d05f
LauraProbst <lauraskprobst@gmail.com>
2020-04-15 15:46:32 +0200

    .

--- javascript/striber.js
+++ javascript/striber.js
@@ -10,9 +10,27 @@ function changeColor(element, color){
 
 function myFunction() {
   alert("Glæd dig til mange flere lækre billeder.De er på vej!");
-
 }
-
 //Arry slut//
 
 //Loops//
+
+
+//If or Else//
+var x = 5;
+var y = 10;
+var z = 17;
+if(x > y && x > z)
+   alert("X er størst");
+else if (y > x && y < z)
+   alert("Her vil du finde information om Matas Striber produkterne");
+// slut if or els//
+
+//operatore//
+
+//var x = 250;
+//var y = 700;
+//var z = x + y;
+//document.getElementById("Matasprodukter").innerHTML = z;
+
+//slut operatore
